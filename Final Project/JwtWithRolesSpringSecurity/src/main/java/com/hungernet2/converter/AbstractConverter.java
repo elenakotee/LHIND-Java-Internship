5a11334b80eacc00f88476f0d5a12051c58d7a21
elenakotee <77975345+elenakotee@users.noreply.github.com>
2022-07-28 08:27:37 -0400

    Delete Final Project/JwtWithRolesSpringSecurity directory

--- Final Project/JwtWithRolesSpringSecurity/src/main/java/com/hungernet2/converter/AbstractConverter.java
+++
@@ -1,7 +0,0 @@
-package com.hungernet2.converter;
-
-public abstract class AbstractConverter<ENTITY, DTO> {
-
-	public abstract ENTITY toEntity(DTO dto);
-	public abstract DTO toDTO(ENTITY entity);
-}
